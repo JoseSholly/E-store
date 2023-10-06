d4d3ba53ffb21cd027fcb63612ecbf19c6187f54
JoseSholly <josepholawuyi30@gmail.com>
2023-10-06 08:22:53 +0100

    add method to class Cart

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -12,3 +12,17 @@ class Cart:
             # save an empty cart in the session
             cart= self.session[settings.CART_SESSION_ID]={}
         self.cart= cart
+    def add(self, product, quantity=1, override_quantity=False):
+        # Add a product tpo the cart or update the quantity
+        product_id= str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id]= {'quantity': 0,
+                                    'price': str(product.price)}
+        if override_quantity:
+            self.cart[product_id]['quantity']= quantity
+        else:
+            self.cart[product_id]['quantity']+=quantity
+    def save(self):
+        # mark thesessio as 'modified' to make sure it gets saved
+        self.session.modified= True
+        
