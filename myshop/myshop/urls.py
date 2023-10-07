d3afb13bf1a9c1386b33e6c4db44f3e7de6d61a1
JoseSholly <josepholawuyi30@gmail.com>
2023-10-07 17:55:24 +0100

    adding url to admin

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
      path('__debug__/', include('debug_toolbar.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
+    path('orders/', include('orders.urls', namespace='orders')),
+    
 
 ]
 
