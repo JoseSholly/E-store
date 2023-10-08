b911fc6008630a5499efcc70a5047cd234c1359d
JoseSholly <josepholawuyi30@gmail.com>
2023-10-08 23:21:51 +0100

    Setting up Birthday field for user model

--- myshop/users/models.py
+++ myshop/users/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as lazy
+from birthday import BirthdayField
 
 # Create your models here.
 class CustomUser(AbstractUser):
     username= None
     email= models.EmailField(lazy('email address'), unique=True)
+    first_name= models.CharField(max_length=100)
+    middle_name= models.CharField(max_length=100)
+    last_name= models.CharField(max_length=100)
+    date_of_birth= models.BirthdayField(null=True, blank=True)
+    
+
+
+
     USERNAME_FIELD= 'email'
     REQUIRED_FIELDS= []
 
