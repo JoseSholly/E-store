7520520f247813d2349a323c441adc34a9bb7a26
JoseSholly <josepholawuyi30@gmail.com>
2023-10-08 23:45:37 +0100

    adding user and profile model to admin

--- myshop/users/admin.py
+++ myshop/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CustomProfile, CustomUser
 
 # Register your models here.
+admin.site.register(CustomProfile)
+admin.site.register(CustomUser)
--- myshop/users/models.py
+++ myshop/users/models.py
@@ -9,16 +9,20 @@ class CustomUser(AbstractUser):
     username= None
     email= models.EmailField(lazy('email address'), unique=True)
     first_name= models.CharField(max_length=100)
-    middle_name= models.CharField(max_length=100)
     last_name= models.CharField(max_length=100)
-    date_of_birth= models.BirthdayField(null=True, blank=True)
+    date_of_birth= BirthdayField(null=True, blank=True)
     phone_number = PhoneNumberField(blank=True, null=True)
-
-
-
 
     USERNAME_FIELD= 'email'
     REQUIRED_FIELDS= []
 
     def __str__(self) -> str:
         return  self.email
+    
+class CustomProfile(models.Model):
+    user= models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    image= models.ImageField(upload_to= 'profile_pics')
+
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name} Profile"
