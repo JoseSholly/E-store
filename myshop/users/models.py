9c583c883a9acd592e723a09846d5535fafc1709
JoseSholly <josepholawuyi30@gmail.com>
2023-10-08 22:17:51 +0100

    Create custom user model and fixing bugs

--- myshop/users/models.py
+++ myshop/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as lazy
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    username= None
+    email= models.EmailField(lazy('email address'), unique=True)
+    USERNAME_FIELD= 'email'
+    REQUIRED_FIELDS= []
+
+    def __str__(self) -> str:
+        return  self.email
