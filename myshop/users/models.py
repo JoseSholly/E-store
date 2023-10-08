cf317115f34ca3a29d19c39c5446f35f3f8a9c6d
JoseSholly <josepholawuyi30@gmail.com>
2023-10-09 00:15:46 +0100

    Restructuring user model

--- myshop/users/models.py
+++ myshop/users/models.py
@@ -1,28 +1,62 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as lazy
 from birthday import BirthdayField
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    username= None
-    email= models.EmailField(lazy('email address'), unique=True)
-    first_name= models.CharField(max_length=100)
-    last_name= models.CharField(max_length=100)
-    date_of_birth= BirthdayField(null=True, blank=True)
-    phone_number = PhoneNumberField(blank=True, null=True)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    USERNAME_FIELD= 'email'
-    REQUIRED_FIELDS= []
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-    def __str__(self) -> str:
-        return  self.email
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser, PermissionsMixin):
+    email = models.EmailField(unique=True, null=False, default='admin@gmail.com')
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    username = models.CharField(max_length=150, unique=True, default='')
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
     
 class CustomProfile(models.Model):
     user= models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     image= models.ImageField(upload_to= 'profile_pics')
+    date_of_birth= BirthdayField(null=True, blank=True)
+    phone_number = PhoneNumberField(blank=True, null=True)
 
 
     def __str__(self) -> str:
         return f"{self.user.first_name} {self.user.last_name} Profile"
+    
+
+
+
+
+
+
