b3ae88e1f73b528e67b710be152018d33370580c
JoseSholly <josepholawuyi30@gmail.com>
2023-11-02 23:27:19 +0100

    Adding asynchronous tasksand configuring email

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import dotenv_values, load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -165,3 +166,11 @@ LOGOUT_REDIRECT_URL = 'shop:product_list'
 
 
 
+# Email server configuration
+load_dotenv()
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASS")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
