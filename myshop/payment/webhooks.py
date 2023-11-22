ab390a428ed3ce2923f71c3f8ccf8372c0580cc1
JoseSholly <josepholawuyi30@gmail.com>
2023-11-22 23:07:43 +0100

    Trying to fix webhook order.paid bug

--- myshop/payment/webhooks.py
+++ myshop/payment/webhooks.py
@@ -1,43 +1,3 @@
-# import stripe
-# from django.conf import settings
-# from django.http import HttpResponse
-# from django.views.decorators.csrf import csrf_exempt
-# from orders.models import Order
-
-
-# @csrf_exempt
-# def strife_webhook(request):
-#     payload= request.body
-#     sig_header= request.META['HTTP_STRIPE_SSIGNATURE']
-#     event= None
-
-#     try:
-#         event= stripe.Webhook.construct_event(
-#             payload,
-#             sig_header,
-#             settings.STRIPE_WEBHOOK_SECRET
-#         )
-#     except ValueError as e:
-#         # Invalid payload
-#         return HttpResponse(status= 400)
-#     except stripe.error.SignatureVerificationError as e:
-#         # Invaid Signature
-#         return HttpResponse(status=400)
-#     if event.type == "checkout.session.completed":
-#         session= event.data.object
-#         if session.mode =='payment' and session.payment_status == 'paid':
-#             try:
-#                 order = Order.objects.get(id=event.data.client_reference_id)
-#             except Order.DoesNotExist:
-#                 return HttpResponse(404)
-#             # mark order as paid
-#             order.paid = True
-#             order.save()
-
-    
-#     return HttpResponse(status=200)
-
-
 import stripe
 from django.conf import settings
 from django.http import HttpResponse
@@ -47,37 +7,34 @@ from orders.models import Order
 
 @csrf_exempt
 def stripe_webhook(request):
-    payload = request.body
+    payload= request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-    event = None
+    event= None
 
     try:
-        event = stripe.Webhook.construct_event(
+        event= stripe.Webhook.construct_event(
             payload,
             sig_header,
             settings.STRIPE_WEBHOOK_SECRET
         )
     except ValueError as e:
         # Invalid payload
-        return HttpResponse(status=400)
+        return HttpResponse(status= 400)
     except stripe.error.SignatureVerificationError as e:
-        # Invalid Signature
+        # Invaid Signature
         return HttpResponse(status=400)
-
     if event.type == "checkout.session.completed":
-        session = event.data.object
-        if session.mode == 'payment' and session.payment_status == 'paid':
-            handle_payment_success(session.client_reference_id)
+        session= event.data.object
+        if session.mode =='payment' and session.payment_status == 'paid':
+            try:
+                order = Order.objects.get(id=session.client_reference_id)
+                print(order)
+            except Order.DoesNotExist:
+                return HttpResponse(404)
+            # mark order as paid
+            order.paid = True
+            order.save()
 
+    
     return HttpResponse(status=200)
 
-
-def handle_payment_success(order_id):
-    try:
-        order = Order.objects.get(id=order_id)
-    except Order.DoesNotExist:
-        return HttpResponse(status=404)
-
-    # Mark order as paid
-    order.paid = True
-    order.save()
