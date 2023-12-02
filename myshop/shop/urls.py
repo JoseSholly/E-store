5497ec03a5586db13e9e0a334028819183953d98
JoseSholly <josepholawuyi30@gmail.com>
2023-12-02 01:50:21 +0100

    Change favorite url

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
     path('', views.product_list, name= 'product_list'),
     path('<slug:category_slug>/', views.product_list,name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail,name='product_detail'), 
-    path('toggle_favorite/<int:product_id>/', views.toggle_favorite,name='toggle_favorite'),
+    # path('toggle_favorite/<int:product_id>/', views.toggle_favorite,name='toggle_favorite'),
     path('<int:id>/<slug:slug>/review', views.post_review, name='post_review'),
     
 
