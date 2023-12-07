12edb07db7118ff72c55bba7544aa05cf0c1f319
JoseSholly <josepholawuyi30@gmail.com>
2023-12-08 00:48:55 +0100

    Create order_id context processor

---
+++ myshop/orders/context_processors.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404
+from .models import Order
+
+
+def order_processor(request):
+    order_id = None
+    if request.user.is_authenticated:
+        try:
+            # Get order from user's session
+            order_id = request.session['order_id']
+        except KeyError:
+            # Get latest order for user
+            try:
+                order = get_object_or_404(Order, user=request.user, paid=False)
+                order_id = order.id
+                request.session['order_id'] = order_id
+            except Order.DoesNotExist:
+                pass
+    return {'order_id': order_id}
