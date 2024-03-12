c612ac7676a3a699cdc62ccdeedbce14b43a2b5c
JoseSholly <josepholawuyi30@gmail.com>
2024-03-12 19:25:59 +0100

    Postgres Switch

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -94,8 +94,16 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jose_store',
+        'USER': 'jose_store',
+        'PASSWORD': 'TESTING321',
+        'HOST': 'localhost',
+
+        
     }
 }
 
