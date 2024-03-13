65dd87cea17523fb3dfe0e348e68a0b78e0b15ef
JoseSholly <josepholawuyi30@gmail.com>
2024-03-13 02:35:43 +0100

    Add: uuid field to order

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -24,77 +24,81 @@ def validate_discount(value):
         raise ValidationError("Discount must be between 0 and 100.")
     else:
         return value
-    
+
 # Create your models here.
+
 
 class Order(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4,
-                               editable=False)
-    first_name= models.CharField(max_length=50,)
-    last_name= models.CharField(max_length=50,)
-    email= models.EmailField()
-    address= models.CharField(max_length=250)
-    postal_code= models.CharField(max_length=10,null=True,validators=[postal_code_validator])
-    city= models.CharField(max_length=100)
-    created= models.DateTimeField(auto_now_add=True)
-    updated= models.DateTimeField(auto_now=True)
-    paid= models.BooleanField(default=False)
+                          unique=True,
+                          editable=False)
+    first_name = models.CharField(max_length=50,)
+    last_name = models.CharField(max_length=50,)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(
+        max_length=10, null=True, validators=[postal_code_validator])
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
     coupon = models.ForeignKey(Coupon,
                                related_name="orders_coupon",
                                null=True,
                                blank=True,
-                               on_delete= models.SET_NULL)
+                               on_delete=models.SET_NULL)
     discount = models.IntegerField(default=0,
                                    validators=[validate_discount])
     stripe_id = models.CharField(max_length=250, blank=True)
 
-
     class Meta:
-        ordering= ['-created']
-        indexes= [
+        ordering = ['-created']
+        indexes = [
             models.Index(fields=['-created'])
         ]
+
     def __str__(self) -> str:
         return f"{self.id}"
 
     def get_total_cost(self):
-        total_cost= self.get_total_cost_before_discount()
+        total_cost = self.get_total_cost_before_discount()
         return total_cost - self.get_discount()
-    
+
     def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
-    
+
     def get_discount(self):
-        total_cost= self.get_total_cost_before_discount()
+        total_cost = self.get_total_cost_before_discount()
         if self.discount:
             return total_cost * (self.discount / Decimal(100))
         return Decimal(0)
+
     def get_stripe_url(self):
         if not self.stripe_id:
             # no payment associated
             return ''
         if "_test_" in settings.STRIPE_SECRET_KEY:
             # Stripe path for test payments
-            path= "/test/"
+            path = "/test/"
         else:
             # Stripe path for real payments
-            path= '/'
+            path = '/'
         return f"https://dashboard.stripe.com{path}payments/{self.stripe_id}"
-    
+
 
 class OrderItem(models.Model):
-    order= models.ForeignKey(Order,
-                             related_name='items',
-                             on_delete=models.CASCADE)
-    product= models.ForeignKey(Product,
-                               related_name='order_items',
-                               on_delete=models.CASCADE)
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10,
-                                decimal_places=2,default=0)
-    quantity= models.PositiveIntegerField(default=1)
-    
+                                decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self) -> str:
         return str(self.id)
+
     def get_cost(self):
         return self.price * self.quantity
